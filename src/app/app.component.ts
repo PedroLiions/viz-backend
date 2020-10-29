2e30f29a124ae12799a5960f100d45fb9b476a31
Pedro Lima <pedrolion1@hotmail.com>
2020-10-29 00:37:50 -0300

    remove ngx-bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,8 @@
 import { Component } from '@angular/core';
-import { setTheme} from 'ngx-bootstrap/utils';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  constructor() {
-    setTheme('bs4');
-  }
-}
+export class AppComponent {}
